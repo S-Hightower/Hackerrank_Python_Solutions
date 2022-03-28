a49f252cd981730e4127d0d1e94c275f01b5535a
Shawna Hightower <sdhightower6@gmail.com>
2022-03-28 11:22:23 -0600

    algo added

---
+++ breakingTheRecords.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'breakingRecords' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY scores as parameter.
+#
+
+def breakingRecords(scores):
+    
+    min = scores[0]
+    max = scores[0]
+    
+    min_record = 0
+    max_record = 0
+    
+    for index in range(1, len(scores)):
+        if min > scores[index]:
+            min = scores[index]
+            min_record += 1
+        
+        elif max < scores[index]:
+            max = scores[index]
+            max_record += 1
+            
+    return max_record, min_record
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    scores = list(map(int, input().rstrip().split()))
+
+    result = breakingRecords(scores)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
